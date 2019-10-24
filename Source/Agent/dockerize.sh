f69b99b09cced4254a2ca35f7c9bd5cd97ad2217
Einar Ingebrigtsen <einar@dolittle.com>
2019-10-24 13:44:41 +0200

    Using the version number from the repo

--- Source/Agent/dockerize.sh
+++ Source/Agent/dockerize.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
+export VERSION=$(git tag --sort=-version:refname | head -1)
 docker build -t dolittle/azure-devops-build-agent .
+docker tag dolittle/azure-devops-build-agent dolittle/azure-devops-build-agent:$VERSION
