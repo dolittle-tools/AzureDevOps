7a382bff237a620410c0461c5a473df6325710a9
Sindre Wilting <sindrewilting@gmail.com>
2020-05-14 10:02:33 +0200

    create PATCH variable and remove prefixed v from semantic version

--- Source/Agent/patch.sh
+++ Source/Agent/patch.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 export VERSION=$(git tag --sort=-version:refname | head -1)
+PATCH='{"spec": { "template": { "spec": { "containers": [{ "name":"build-agent", "image": "dolittle/$"}] }}}}'
+if [[ "$VERSION" = \v* ]] ; then
+    VERSION=${VERSION:1}
+fi
 kubectl patch --namespace application-79a5f2f8-bd91-de33-4e9b-73b87b418124 statefulset azure-devops-build-agent -p "${PATCH//\$/azure-devops-build-agent:$VERSION}"
